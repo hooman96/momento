1e9c2bf24dc5ff5902547d527eeea091fb31f92c
uchiha_vivek <vs160125@gmail.com>
2024-06-30 09:33:17 +0530

    review the errors of blog-page branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}',
+      './public/index.html',
+    ],
+    options: {
+      safelist: [],
+    },
+  },
+  darkMode: 'class', // or 'media'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 };
+
